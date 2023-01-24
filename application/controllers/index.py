2cf25423a6f7e621f38ee99652dc46af600ba0aa
jonathanGea <jonathangea09@gmail.com>
2023-01-24 09:30:26 +0700

    json

--- application/controllers/index.py
+++ application/controllers/index.py
@@ -1,7 +1,31 @@
 from application import app
-from flask import render_template, url_for, request, redirect, flash, session
+from flask import render_template, url_for, request, redirect, flash, session, jsonify
 
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+
+@app.route('/index')
+def home():
+    data = {
+        "id": 1,
+        "title": "iPhone 9",
+        "description": "An apple mobile which is nothing like apple",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "stock": 94,
+        "brand": "Apple",
+        "category": "smartphones",
+        "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        "images": [
+            "https://i.dummyjson.com/data/products/1/1.jpg",
+            "https://i.dummyjson.com/data/products/1/2.jpg",
+            "https://i.dummyjson.com/data/products/1/3.jpg",
+            "https://i.dummyjson.com/data/products/1/4.jpg",
+            "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+        ]
+    }
+    return jsonify(data)
